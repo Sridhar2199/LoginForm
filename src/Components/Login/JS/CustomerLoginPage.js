decb7516f8df1c45a2e46fcccf5e47ee027183ac
Sridhar <sridhar2199n@gmail.com>
2022-09-27 18:15:05 +0530

    Adding Email and password

--- src/Components/Login/JS/CustomerLoginPage.js
+++ src/Components/Login/JS/CustomerLoginPage.js
@@ -1,14 +1,19 @@
-import "../CSS/CustomerLoginPage.css";
-import LoginImage from "../../../Resources/Images/CustomerLoginPage.jpg";
 import LoginForm from "./LoginForm";
+
+import "../CSS/CustomerLoginPage.css";
+
+import LoginImage from "../../../Resources/Images/CustomerLoginPage.jpg";
+import ProductInfo from "../../../Resources/Images/ProductInfo.png";
+
 
 function CustomerLoginPage() {
   return (
     <div className="login-container">
-      <img src={LoginImage} alt="LoginPageImage" className="login-image"></img>
-      <div className="login-form">
-        <LoginForm />
-      </div>
+        <img src={LoginImage} alt="LoginPageImage" className="login-image"></img>
+        <div className="login-form">
+                <img src={ProductInfo} alt="ProductInfo" className="product-image"></img>
+                <LoginForm />
+        </div>
     </div>
   );
 }
--- src/Components/Login/JS/LoginForm.js
+++ src/Components/Login/JS/LoginForm.js
@@ -5,7 +5,8 @@ function LoginForm() {
     return (
         <div class="child-login-form">   
                 <input type="email" placeholder="Email" name="Email" id="child-login-email"/>               
-                <input type="password" placeholder="Password" name="Password" id="child-login-password"/>          
+                <input type="password" placeholder="Password" name="Password" id="child-login-password"/>
+                <button type="submit" id="child-login-proceed">Proceed</button>          
         </div>
     )
 }
